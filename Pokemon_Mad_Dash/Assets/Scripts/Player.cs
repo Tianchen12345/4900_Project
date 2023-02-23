6ebebf6c345710faed6967c06e052a04ba410ce3
zyaire11 <102438276+zyaire11@users.noreply.github.com>
2023-02-23 15:43:34 -0500

    fix the jumping problem and attack animation, we can plus z to attack

--- Pokemon_Mad_Dash/Assets/Scripts/Player.cs
+++ Pokemon_Mad_Dash/Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,18 +7,34 @@ using UnityStandardAssets.CrossPlatformInput;
 public class Player : MonoBehaviour
 {
     [SerializeField] float runSpeed = 10f;
+    [SerializeField] float jumpSpeed = 9f;
     Rigidbody2D myRigidbody2D;
+    Animator myAnimator;
 
     // Start is called before the first frame update
     void Start()
     {
         myRigidbody2D = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Run();
+        ChangingToAttackState();
+        Jump();
+    }
+
+    private void Jump()
+    {
+        bool isJumping = CrossPlatformInputManager.GetButtonDown("Jump");
+
+        if (isJumping)
+        {
+            Vector2 jumpVelocity = new Vector2(myRigidbody2D.velocity.x, jumpSpeed);
+            myRigidbody2D.velocity = jumpVelocity;
+        }
     }
 
     // Running is a method for running
@@ -28,11 +45,26 @@ public class Player : MonoBehaviour
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidbody2D.velocity.y);
         myRigidbody2D.velocity = playerVelocity;
 
-        flipSprites();
+        FlipSprites();
+
+        ChangingToRunningState();
+        ChangingToAttackState();
+    }
+
+    private void ChangingToAttackState()
+    {
+        bool pressAttackButton = Input.GetKeyDown(KeyCode.Z);
+        myAnimator.SetBool("Attack", pressAttackButton);
+    }
+
+    private void ChangingToRunningState()
+    {
+        bool runningHorizontally = Mathf.Abs(myRigidbody2D.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("Running", runningHorizontally);
     }
 
     // make the sprites flip when user press left button
-    private void flipSprites()
+    private void FlipSprites()
     {
         bool runningHorizontally = Mathf.Abs(myRigidbody2D.velocity.x) > Mathf.Epsilon;
 
