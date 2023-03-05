b9bba178d200c87b88d9a55253c7db36df556fc3
zyaire11 <102438276+zyaire11@users.noreply.github.com>
2023-03-05 16:50:36 -0500

    Add sound effects for hearts and diamonds

--- Pokemon_Mad_Dash/Assets/Scripts/DiamondPickup.cs
+++ Pokemon_Mad_Dash/Assets/Scripts/DiamondPickup.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class DiamondPickup : MonoBehaviour
 {
+    [SerializeField] AudioClip diamondSFX;
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        AudioSource.PlayClipAtPoint(diamondSFX, Camera.main.transform.position);
         Destroy(gameObject);
     }
 }
