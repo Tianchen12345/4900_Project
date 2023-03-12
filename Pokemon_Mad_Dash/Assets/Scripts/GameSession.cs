fb6eb2db86a0e2bc1159f09bb8a4267c0015f91d
zyaire11 <102438276+zyaire11@users.noreply.github.com>
2023-03-12 12:54:10 -0400

    added a boss level

--- Pokemon_Mad_Dash/Assets/Scripts/GameSession.cs
+++ Pokemon_Mad_Dash/Assets/Scripts/GameSession.cs
@@ -59,12 +59,6 @@ public class GameSession : MonoBehaviour
         else
         {
             resetGame();
-            //gameOverScreen.SetActive(true);
-            //playerLives = 3;
-            //updatedHearts();
-            //livesText.text = playerLives.ToString();
-            //Player.SetActive(false);
-
         }
     }
 
