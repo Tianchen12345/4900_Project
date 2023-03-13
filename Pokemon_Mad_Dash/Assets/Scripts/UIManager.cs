74db3aa92cb73febbc961af70a45fae034b6729d
zyaire11 <102438276+zyaire11@users.noreply.github.com>
2023-03-13 13:55:44 -0400

    Add a pause screen and update main menu screen

--- Pokemon_Mad_Dash/Assets/Scripts/UIManager.cs
+++ Pokemon_Mad_Dash/Assets/Scripts/UIManager.cs
@@ -3,6 +3,18 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    [SerializeField] private GameObject pauseGameScreen;
+
+    private void Awake()
+    {
+        pauseGameScreen.SetActive(false);
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    #region Game Over
     public void Restart()
     {
         SceneManager.LoadScene(1);
@@ -11,10 +23,45 @@ public class UIManager : MonoBehaviour
     public void MainMenu()
     {
         SceneManager.LoadScene(0);
+        pauseGameScreen.SetActive(false);
     }
 
     public void Quit()
     {
         Application.Quit();
     }
+    #endregion
+
+    #region Pause Game
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pauseGameScreen.activeInHierarchy)
+            {
+                PauseGame(false);
+            }
+            else
+            {
+                PauseGame(true);
+            }
+        }
+    }
+
+    public void PauseGame(bool status)
+    {
+        pauseGameScreen.SetActive(status);
+
+        //when pause the game, change timescale to 0 
+        if (status)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+    #endregion
 }
